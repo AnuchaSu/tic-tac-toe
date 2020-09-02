ba3a119b0ef669a8cd1aac7af13ed34fd6b881d9
AnuchaSu <art0913452643@gmail.com>
2020-09-02 10:45:50 +0700

    Updates

---
+++ precache-manifest.7092992f821746faa2dab58e11a2fb1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7a8c931bcc1d519c1ba0b3ad84449a8",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "795795458986cb0080b8",
+    "url": "/tic-tac-toe/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/tic-tac-toe/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "795795458986cb0080b8",
+    "url": "/tic-tac-toe/static/js/main.42533f17.chunk.js"
+  },
+  {
+    "revision": "4ad95f08d6b4d56d4069",
+    "url": "/tic-tac-toe/static/js/runtime-main.fa7aabe4.js"
+  }
+]);
